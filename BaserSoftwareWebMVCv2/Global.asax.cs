2e99464ab4eda3736645630181dbd39bc62e6387
basersoftware <30554032+baseryazilim@users.noreply.github.com>
2020-06-26 14:01:50 +0300

    Initial commit

---
+++ BaserSoftwareWebMVCv2/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Threading;
+using System.Globalization;
+
+namespace BaserSoftwareWebMVCv2
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AcquireRequestState(object sender,EventArgs e)
+        {
+            string culture = "en-US";
+            if (Request.UserLanguages != null)
+            {
+                culture = Request.UserLanguages[0];
+            }
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(culture);
+        }
+    }
+}
